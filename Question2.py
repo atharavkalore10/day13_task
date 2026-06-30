7c35140588a4c672020b2021e9be11f6e4614ebd
atharav <atharavkalore10@gmail.com>
2026-06-30 22:38:12 +0530

    Implement array creation using zeros and ones

---
+++ Question2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+zeros1 = np.zeros(8)
+
+ones = np.ones((4, 4))
+
+zeros2 = np.zeros((3, 3))
+
+print("1D Array of Zeros")
+print(zeros1)
+
+print("\n4x4 Ones Matrix")
+print(ones)
+
+print("\n3x3 Zero Matrix")
+print(zeros2)
