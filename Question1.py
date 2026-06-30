892472d758c19c1b6c0e74e190db68fac3916f12
atharav <atharavkalore10@gmail.com>
2026-06-30 22:29:24 +0530

    Created basic array

---
+++ Question1.py
@@ -0,0 +1,18 @@
+import numpy as np
+# 1d Array
+arr1 = np.array([10, 20, 30, 40, 50])
+
+#2d Array
+arr2 = np.array([[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9]])
+
+print("1D Array:")
+print(arr1)
+print("Shape:", arr1.shape)
+print("Data Type:", arr1.dtype)
+
+print("1D Array:")
+print(arr1)
+print("Shape:", arr1.shape)
+print("Data Type:", arr1.dtype)
