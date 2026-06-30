5765f79c2c3fd54987ceba87db0c31d6b14833f6
atharav <atharavkalore10@gmail.com>
2026-06-30 22:44:55 +0530

    use linspace

---
+++ Question4.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr1 = np.linspace(0, 5, 10)
+
+arr2 = np.linspace(-10, 10, 15)
+
+print(arr1)
+print("Length:", len(arr1))
+
+print()
+
+print(arr2)
+print("Length:", len(arr2))
