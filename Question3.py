c56b002660d2694824d0c7db139e7b47b5091dde
atharav <atharavkalore10@gmail.com>
2026-06-30 22:40:53 +0530

    Generate sequences using NumPy arange

---
+++ Question3.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.arange(0, 21)
+
+b = np.arange(10, 51, 2)
+
+c = np.arange(5, 101, 5)
+
+print("0 to 20")
+print(a)
+
+print("\nEven Numbers")
+print(b)
+
+print("\nMultiples of 5")
+print(c)
