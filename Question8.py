926785c0ed486a69a335d1223f89d021019ef001
atharav <atharavkalore10@gmail.com>
2026-07-01 20:31:20 +0530

    properties and statistics of matrix

---
+++ Question8.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.random.randint(1,101,(4,4))
+
+print(arr)
+
+print("Shape:", arr.shape)
+
+print("Dimension:", arr.ndim)
+
+print("Size:", arr.size)
+
+print("Data Type:", arr.dtype)
+
+print("Minimum:", arr.min())
+
+print("Maximum:", arr.max())
