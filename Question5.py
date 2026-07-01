19a5287e150cd10a262942654a78d236e783163c
atharav <atharavkalore10@gmail.com>
2026-07-01 20:18:07 +0530

    used random function from numpy Library

---
+++ Question5.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.random.rand(10)
+
+b = np.random.randn(3, 3)
+
+c = np.random.randint(10, 51, (4, 5))
+
+print("Random Numbers")
+print(a)
+
+print("\nStandard Normal Matrix")
+print(b)
+
+print("\nRandom Integers")
+print(c)
